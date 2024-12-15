8d19a620ea57c0f6d61455ef3c320058870f3ccb
mattsel <matt.sel04@gmail.com>
2024-12-15 14:57:08 -0500

    Updated to return json for react api

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
 import time
 import pandas as pd
-from quart import Quart, render_template, request, redirect, url_for, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 import plotly.express as px
 from prometheus_client import Counter, Histogram, generate_latest, CollectorRegistry
-from redis.asyncio import Redis
+from redis import Redis
 import pickle
 import zlib
 
-app = Quart(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 
 redis = Redis.from_url(os.getenv("REDIS_URL"), ssl_cert_reqs=None)
 MONGODB_CONNECTION_STRING = os.getenv('CONNECTION_STRING')
@@ -23,9 +23,9 @@ request_counter = Counter('http_requests_total', 'Total number of HTTP requests'
 error_counter = Counter('http_errors_total', 'Total number of HTTP errors', ['method', 'endpoint'], registry=registry)
 latency_histogram = Histogram('http_request_latency_seconds', 'Histogram of HTTP request latency', ['method', 'endpoint'], registry=registry)
 
-async def fetch_data_from_mongodb(stock_symbol):
+def fetch_data_from_mongodb(stock_symbol):
     try:
-        cached_data = await redis.get(stock_symbol)
+        cached_data = redis.get(stock_symbol)
         if cached_data:
             df = pickle.loads(zlib.decompress(cached_data))
             return df
@@ -35,7 +35,7 @@ async def fetch_data_from_mongodb(stock_symbol):
             df = pd.DataFrame(list(cursor))
             if not df.empty:
                 compressed_df = zlib.compress(pickle.dumps(df))
-                await redis.setex(stock_symbol, 60, compressed_df)
+                redis.setex(stock_symbol, 60, compressed_df)
             else:
                 print("No data found")
             return df
@@ -44,47 +44,33 @@ async def fetch_data_from_mongodb(stock_symbol):
 
 
 @app.before_request
-async def before_request():
+def before_request():
     request.start_time = time.time()
 
 @app.after_request
-async def after_request(response):
+def after_request(response):
     request_counter.labels(method=request.method, endpoint=request.path).inc()
     latency = time.time() - request.start_time
     latency_histogram.labels(method=request.method, endpoint=request.path).observe(latency)
     return response
 
 @app.errorhandler(Exception)
-async def handle_exception(e):
+def handle_exception(e):
     error_counter.labels(method=request.method, endpoint=request.path).inc()
     return jsonify({"error": "An error occurred"}), 500
 
-@app.route('/', methods=['GET', 'POST'])
-async def index():
-    error_message = None
-
-    if request.method == 'POST':
-        selected_stock = (await request.form)['stock_symbol']
-
-        if selected_stock in mongo_db.list_collection_names():
-            return redirect(url_for('result', stock_symbol=selected_stock))
-        else:
-            error_message = "Invalid stock symbol entered. Please try again."
-            print("Error: Invalid stock symbol.")
-
-    return await render_template('index.html', error_message=error_message)
-
-@app.route('/result/<stock_symbol>', methods=['GET'])
-async def result(stock_symbol):
+@app.route('/api/result', methods=['POST'])
+def result():
+    stock_symbol = request.get_json().get("stock_symbol", None)
     result_html = None
     plot_div = None
     mse = None
 
     try:
-        stock_data = await fetch_data_from_mongodb(stock_symbol)
+        stock_data = fetch_data_from_mongodb(stock_symbol)
 
         if stock_data.empty:
-            return await render_template('index.html', error_message="No data found for the selected stock symbol.")
+            return jsonify({"error": "An error occurred"})
 
         stock_data['Date'] = pd.to_datetime(stock_data['Date'])
         stock_data = stock_data.sort_values(by='Date')
@@ -116,13 +102,17 @@ async def result(stock_symbol):
 
     except Exception as e:
         print(f"Error in result function: {e}")
-        return await render_template('index.html', error_message="Invalid stock symbol entered. Please try again.")
+        return jsonify({"error": "An error occurred"})
 
-    return await render_template('result.html', result_html=result_html, plot_div=plot_div,
-                                 mse=mse, selected_stock=stock_symbol)
+    return jsonify({
+        'result_html': result_html,
+        'plot_div': plot_div,
+        'mse': mse,
+        'selected_stock': stock_symbol
+    })
 
 @app.route('/health', methods=['GET'])
-async def health_check():
+def health_check():
     status = {"status": "healthy"}
     try:
         mongo_client.server_info()
@@ -132,7 +122,7 @@ async def health_check():
     return jsonify(status), 200 if 'mongodb' not in status else 500
 
 @app.route('/metrics', methods=['GET'])
-async def metrics():
+def metrics():
     return generate_latest(registry), 200, {'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'}
 
 if __name__ == '__main__':
